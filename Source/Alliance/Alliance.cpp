f3c2e170436999ddada6d6279f4ed176a119a9e2
JUAN JOSE CORROTO MARTIN <JuanJose.Corroto@alu.uclm.es>
2018-05-29 16:23:25 +0200

    Project

---
+++ Source/Alliance/Alliance.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "Alliance.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Alliance, "Alliance" );
+ 

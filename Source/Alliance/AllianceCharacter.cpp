6028594373a5da0eac4cccecbc77e93ea1ddfc82
Unknown <jcordobarom@gmail.com>
2018-06-22 15:11:18 +0200

    averrrr3
    a

--- Source/Alliance/AllianceCharacter.cpp
+++ Source/Alliance/AllianceCharacter.cpp
@@ -55,13 +55,14 @@ Sprint{ 1200.f }, LaunchForce{ 1.f }, LaunchHeight{ 1.f }, Combo{ 0 }, Health{ 1
 
 void AAllianceCharacter::BeginPlay()
 {
-	/*UE_LOG(LogTemp, Error, TEXT("Called begin play"));
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Error, TEXT("Called begin play, Server: %d"), GIsServer);
 	AController* Controller = GetController();
 	AAlliancePlayerController* PlayerController = Cast<AAlliancePlayerController>(Controller);
 
 	UE_LOG(LogTemp, Error, TEXT("Obtained controller: %p"), Controller);
 	if (PlayerController)
-		PlayerController->OnClientLogin();*/
+		PlayerController->OnClientLogin();
 }
 
 void AAllianceCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)

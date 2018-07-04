904ba38abc44b48c4e5589ce5d5ed983b5c5fe8f
JUAN JOSE CORROTO MARTIN <JuanJose.Corroto@alu.uclm.es>
2018-07-04 17:46:46 +0200

    Added secondary attack for morten

---
+++ Source/Alliance/Grenade.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine.h"
+#include "GameFramework/Actor.h"
+#include "Grenade.generated.h"
+
+UCLASS()
+class ALLIANCE_API AGrenade : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AGrenade();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY()
+		UStaticMeshComponent* StaticMeshComponent;
+	UPROPERTY()
+		UProjectileMovementComponent* PMovementComponent;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Function called when timer ends and grenade explodes
+	UFUNCTION()
+		void Explode();
+
+private:
+	float Dmg;
+	float Timer;
+	
+};

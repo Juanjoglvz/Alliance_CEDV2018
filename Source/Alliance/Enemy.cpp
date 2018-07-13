0cb7ba044fa3dd2028af43ed2ff47730d1a9aba0
JUAN JOSE CORROTO MARTIN <JuanJose.Corroto@alu.uclm.es>
2018-07-13 13:20:34 +0200

    Changed enemies so that they also have an state machine instead of booleans

--- Source/Alliance/Enemy.cpp
+++ Source/Alliance/Enemy.cpp
@@ -5,7 +5,7 @@
 
 
 // Sets default values
-AEnemy::AEnemy() 
+AEnemy::AEnemy() : CurrentState{EEnemyState::S_Idle}
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;

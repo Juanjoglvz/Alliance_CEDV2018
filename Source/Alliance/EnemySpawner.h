d793909522d021fcec94dc0c300ee5618f5c2d14
Unknown <jcordobarom@gmail.com>
2018-09-10 22:10:21 +0200

    Fix offset byte warning

--- Source/Alliance/EnemySpawner.h
+++ Source/Alliance/EnemySpawner.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2018 Iván García, Juan José Corroto and Javier Córdoba - All Rights Reserved
+/* Copyright (C) 2018 Ivan Garcia, Juan Jose Corroto and Javier Cordoba - All Rights Reserved
 * You may use, distribute and modify this code under the
 * terms of the GNU GPLv3 license.
 *

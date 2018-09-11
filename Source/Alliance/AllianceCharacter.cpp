ed1b706c12df98b16b3f7746504101bf76453ae5
Unknown <jcordobarom@gmail.com>
2018-09-11 12:05:21 +0200

    Characters only take damage if controlled by a player

--- Source/Alliance/AllianceCharacter.cpp
+++ Source/Alliance/AllianceCharacter.cpp
@@ -297,6 +297,9 @@ void AAllianceCharacter::MoveRight(float Value)
 
 float AAllianceCharacter::TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
 {
+	if (!GetController()->IsA((UClass*)AAlliancePlayerController::StaticClass)) // If character is not controlled by a player then do not receive damage
+		return 0;
+
 	float ActualDamage = Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
 
 	if (ActualDamage > 0.f && !b_IsEvading)
